2063acdfda640d46595fdeb7527a4a1901f98e78
Toru Maesaka <dev@torum.net>
2011-06-16 18:54:52 +0900

    Define Kyoto Tycoon errors.

--- kyototycoon/kt_error.py
+++ kyototycoon/kt_error.py
@@ -8,20 +8,42 @@
 class KyotoTycoonError:
     SUCCESS  = 0
     NOIMPL   = 1
-    IMVALID  = 2
+    INVALID  = 2
     LOGIC    = 3
     INTERNAL = 4
     NETWORK  = 5
-    EMISK    = 15
+    EMISC    = 255
+
+    ErrorNameDict = {
+        SUCCESS: "SUCCESS",
+        NOIMPL: "UNIMPLEMENTED",
+        INVALID: "INVALID",
+        LOGIC: "LOGIC",
+        INTERNAL: "INTERNAL",
+        NETWORK: "NETWORK",
+        EMISC: "EMISC",
+    }
+
+    ErrorMessageDict = {
+        SUCCESS: "Operation Successful",
+        NOIMPL: "Unimplemented Operation",
+        INVALID: "Invalid Operation",
+        LOGIC: "Logic Error",
+        INTERNAL: "Internal Error",
+        NETWORK: "Network Error",
+        EMISC: "Miscellenious Error",
+    }
 
     def __init__(self):
         self.error_code = self.SUCCESS
+        self.error_name = self.ErrorNameDict[self.SUCCESS]
+        self.error_message = self.ErrorMessageDict[self.SUCCESS]
 
     def code(self):
         return self.error_code
 
     def name(self):
-        pass
+        return self.error_name
 
     def message(self):
-        pass
+        return self.error_message
